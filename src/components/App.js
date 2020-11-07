32eca83c17a938b17d4bd2c2a7f1d52adaec6a7a
danieljaeim <imdanieljae@gmail.com>
2020-11-06 17:52:43 -0800

    pushing starter code

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+// REGULAR IMPORTS
+import React, {Component} from 'react';
+
+// STYLESHEETS
+import '../css/App.css';
+
+// STATIC INFO JSON FILES
+import champdata from '../data/champions.json'
+import itemdata from '../data/items.json'
+
+class App extends React.Component {
+
+  state = {}
+
+  render() {
+    return <div></div>
+  }
+}
+
+export default App;
