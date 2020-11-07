fb0707e63cedb0469d3ac575cec5b29420adc3ea
Robert Plata <norbertedguy@gmail.com>
2020-11-07 16:57:46 -0500

    Added dropdown levels, image list

--- src/containers/Stats.js
+++ src/containers/Stats.js
@@ -1,10 +1,38 @@
 // REGULAR IMPORTS
 import React from 'react';
+import { Dropdown } from 'semantic-ui-react';
 
 // STYLESHEETS
 import '../css/Stats.css';
 
 class Stats extends React.Component {
+    state = {
+        levelOptions: null,
+        currentLevel: 1
+    }
+    images = ["https://i.imgur.com/phYfxhw.png", "https://i.imgur.com/VwNC3Gx.png", "https://i.imgur.com/jbm413s.png"
+        ,"https://i.imgur.com/1a2gqd6.png", "https://i.imgur.com/HpHBuzr.png", "https://i.imgur.com/YGLWVBm.png", "https://i.imgur.com/O6G2qo5.png", "https://i.imgur.com/rC7N7BE.png",
+        "https://i.imgur.com/BICPlMn.png", "https://i.imgur.com/4unvtif.png"];
+    
+    componentDidMount = () => {
+        this.setState({levelOptions: this.levels()})
+    }
+
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+
+    levels = () => {
+        var arr = []
+        for (var level = 1; level <= 18; level++) {
+            arr.push({key: level, value: level, text: "Level: " + level})
+        } 
+        return arr;
+    }
+    
+    handleDropdownChange = (event, data) => {
+        this.setState({level: data.value})
+    }
 
     getRoles() {
         var roles = this.props.roles.join(" ").toLowerCase().split(" ");
@@ -16,65 +44,32 @@ class Stats extends React.Component {
     }
 
     render() {
-
+        const { levelOptions } = this.state;
         return (
             <>
+            <Dropdown
+                placeholder='Level 1'
+                onChange={this.handleDropdownChange}
+                fluid
+                search
+                selection
+                options={levelOptions}
+            />
                 <div className="role" >
-                    Roles: {this.getRoles()} 
+                    Roles: {this.getRoles()}
                 </div>
                 <div className="level-stat" >
                     Level: {this.props.currentStats.level} 
                 </div>
-                {Object.keys(this.props.currentStats).map(stat => {
+                {Object.keys(this.props.currentStats).map((stat, index) => {
                     if (stat === 'level') return;
                     return (
                     <div className={`${stat} stat`} >
-                        <img src="https://i.imgur.com/O6G2qo5.png" width="15px" height="15px" style={{textDecoration: 'capitalize'}}/>
+                        <img src={this.images[index]} width="15px" height="15px" style={{textDecoration: 'capitalize'}}/>
                             {stat}: {this.props.currentStats[stat]}
                     </div>
                     )
                 })}
-
-                {/* <div className="Attack stat" >
-                    <img src="https://i.imgur.com/O6G2qo5.png" width="15px" height="15px" />
-            Physical Damage: {this.state.currentStats.attackDamage.flat}
-                </div>
-                <div className="Health stat" >
-                    <img src="https://i.imgur.com/phYfxhw.png" width="15px" height="15px" />
-            Health: {this.state.currentStats.health.flat}
-                </div>
-                <div className="HealthRegen stat" >
-                    <img src="https://i.imgur.com/VwNC3Gx.png" width="15px" height="15px" />
-            Health Regen: {this.state.currentStats.healthRegen.flat}
-                </div>
-                <div className="Mana stat" >
-                    <img src="https://i.imgur.com/jbm413s.png" width="15px" height="15px" />
-            Mana: {this.state.currentStats.mana.flat}
-                </div>
-                <div className="ManaRegen stat" >
-                    <img src="https://i.imgur.com/1a2gqd6.png" width="15px" height="15px" />
-            Mana Regen: {this.state.currentStats.manaRegen.flat}
-                </div>
-                <div className="Armor stat" >
-                    <img src="https://i.imgur.com/HpHBuzr.png" width="15px" height="15px" />
-            Armor: {this.state.currentStats.armor.flat}
-                </div>
-                <div className="MagicResistance stat" >
-                    <img src="https://i.imgur.com/YGLWVBm.png" width="15px" height="15px" />
-            Magic Resistance: {this.state.currentStats.magicResistance.flat}
-                </div>
-                <div className="Move Speed stat" >
-                    <img src="https://i.imgur.com/rC7N7BE.png" width="15px" height="15px" />
-            Move Speed: {this.state.currentStats.movespeed.flat}
-                </div>
-                <div className="AttackRange stat" >
-                    <img src="https://i.imgur.com/4unvtif.png" width="15px" height="15px" />
-            Range: {this.state.currentStats.attackRange.flat}
-                </div> */}
-                {/* <div className="CriticalStrike stat" >
-                    <img src="https://i.imgur.com/BICPlMn.png" width="15px" height="15px" />
-            Critical Strike: {this.state.currentStats.criticalStrikeDamage.flat}
-                </div> */}
             </>
         )
     }
